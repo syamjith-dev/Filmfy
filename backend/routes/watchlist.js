41a0f85ac4577c5e63d7e7333564c892428b9d23
syamjith <syamjithloq@gmail.com>
2026-06-16 21:43:14 +0530

    updated v.0.5

--- backend/routes/watchlist.js
+++ backend/routes/watchlist.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const Watchlist = require("../models/watchlist");
-const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("../middleware/authmiddleware");
 
 
 // ======================
