509fdf71eb8148e27751f3e789d71419a0124a88
syamjith <syamjithloq@gmail.com>
2026-06-19 07:57:44 +0530

    updated remove err

--- backend/server.js
+++ backend/server.js
@@ -33,3 +33,12 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://filmfy.netlify.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
