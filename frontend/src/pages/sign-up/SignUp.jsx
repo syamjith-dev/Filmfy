b159779faae3b1ad9648e0a9436cae30fae92216
syamjith <syamjithloq@gmail.com>
2026-07-02 21:08:11 +0530

    updated the email err fixing

--- frontend/src/pages/sign-up/SignUp.jsx
+++ frontend/src/pages/sign-up/SignUp.jsx
@@ -28,7 +28,7 @@ const SignUp = () => {
     try {
 
        await axios.post(
-        'https://cineverse-5xo9.onrender.com/api/auth/signup',
+        'http://cineverse-5xo9.onrender.com/api/auth/signup',
         {
           name: userData.firstName,
           email: userData.email,
