69fb44affc674bc9be9129e89f20f381766c91b4
syamjith <syamjithloq@gmail.com>
2026-07-03 21:57:10 +0530

    fixing the email authenticaion err

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -4,15 +4,10 @@ const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
-  requireTLS: true,
   auth: {
     user: process.env.BREVO_USER,
     pass: process.env.BREVO_SMTP_KEY,
   },
-
-  connectionTimeout: 30000,
-  greetingTimeout: 30000,
-  socketTimeout: 30000,
 });
 
 transporter.verify((error, success) => {
