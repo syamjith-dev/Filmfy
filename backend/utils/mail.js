819067e072385da759f9feb0f644a609043ae1a9
syamjith <syamjithloq@gmail.com>
2026-07-03 22:48:42 +0530

    testing

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -5,8 +5,8 @@ console.log("PASS exists:", !!process.env.BREVO_PASS);
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.BREVO_USER,
     pass: process.env.BREVO_PASS,
