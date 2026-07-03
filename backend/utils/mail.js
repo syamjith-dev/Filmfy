56cabda015bbef29d0419d2b6d9653e5a06bec92
syamjith <syamjithloq@gmail.com>
2026-07-03 22:42:35 +0530

    emial authentication testing

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -1,12 +1,15 @@
 const nodemailer = require("nodemailer");
 
+console.log("USER:", process.env.BREVO_USER);
+console.log("PASS exists:", !!process.env.BREVO_PASS);
+
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
   auth: {
     user: process.env.BREVO_USER,
-    pass: process.env.BREVO_SMTP_KEY,
+    pass: process.env.BREVO_PASS,
   },
 });
 
@@ -14,7 +17,7 @@ transporter.verify((error, success) => {
   if (error) {
     console.log("SMTP Error:", error);
   } else {
-    console.log("✅ SMTP Server is ready");
+    console.log("SMTP Server is ready");
   }
 });
 
